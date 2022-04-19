fe6d790fb4b5f1210cac7ea60302a2e063c420f6
Breg,Coen C.J <c.breg@student.fontys.nl>
2022-04-19 09:05:30 +0200

    feat: setup for gh-pages deploy

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  env: {
+    PUBLIC_URL: "https://cjbreg.github.io/my-portfolio-next",
+    assetPrefix: "./",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
